bda6523a27e6534d7bb69d93fb2e5a4b75cedd79
Atharvapuranik12 <83014186+Atharvapuranik12@users.noreply.github.com>
2024-06-10 19:42:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import gemini
+import PyPDF2
+
+app = Flask(__name__)
+
+
+def extract_text_from_pdf(file):
+    pdf_reader = PyPDF2.PdfReader(file)
+    text = ''
+    for page_num in range(len(pdf_reader.pages)):
+        text += pdf_reader.pages[page_num].extract_text()
+    return text
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/generate_questions', methods=['POST'])
+def generate_questions():
+    resume_file = request.files['resume']
+    job_description_file = request.files['job_description']
+
+    resume_text = extract_text_from_pdf(resume_file)
+    job_description_text = extract_text_from_pdf(job_description_file)
+    skills = "Nodesjs, Reactsjs, AWS, JavaScript"
+
+    custom_ntquestions = gemini.generate_custom_non_tech_questions(resume_text, job_description_text)
+    custom_tquestions = gemini.generate_custom_tech_questions(skills)
+    return render_template('result.html', custom_questions=custom_ntquestions + "\n\n" + custom_tquestions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
