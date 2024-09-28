6b5a58ddc79cd735327c2cbeff90aef29fff0bcd
Atharvapuranik12 <83014186+Atharvapuranik12@users.noreply.github.com>
2024-09-28 13:19:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def generate_questions():
 
     resume_text = extract_text_from_pdf(resume_file)
     job_description_text = extract_text_from_pdf(job_description_file)
-    skills = "Nodesjs, Reactsjs, AWS, JavaScript"
+    skills = "Machine Learning, Python, Deep Learning,Artificial Intelligence, Large Language Model, Prompt Engineering";
 
     custom_ntquestions = gemini.generate_custom_non_tech_questions(resume_text, job_description_text)
     custom_tquestions = gemini.generate_custom_tech_questions(skills)
